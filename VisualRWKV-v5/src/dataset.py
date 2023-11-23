57d29ad9e95377ab787691dfa6ed1233f87d188d
huohuade <houhaowen@gmail.com>
2023-11-24 00:19:28 +0800

    Update dataset.py

--- VisualRWKV-v5/src/dataset.py
+++ VisualRWKV-v5/src/dataset.py
@@ -2,7 +2,7 @@
 # The RWKV Language Model - https://github.com/BlinkDL/RWKV-LM
 ########################################################################################################
 
-import json, math, random, os, sys, copy
+import json, math, random, os, re, copy
 import numpy as np
 from PIL import Image, ImageFile
 import torch
@@ -21,44 +21,79 @@ def process_image_tokens_in_conversations(
     conversations: Sequence[str],
 ) -> Dict:
     """
-    Process image tokens within conversations by moving the image token to the end of the sentence.
-    which is better for the rkwv to learn.
+    Process image tokens within conversations.
     """
     for sentence in conversations:
         if DEFAULT_IMAGE_TOKEN in sentence['value']:
             sentence['value'] = sentence['value'].replace(DEFAULT_IMAGE_TOKEN, '').strip()
-            sentence['value'] = sentence['value'] + '\n' + DEFAULT_IMAGE_TOKEN
+            sentence['value'] = re.sub(r"\n(\s*\n)+", '\n', sentence['value'])
+            sentence['value'] = DEFAULT_IMAGE_TOKEN + '\n' + sentence['value']
             sentence['value'] = sentence['value'].strip()
+        else:
+            sentence['value'] = re.sub(r"\n(\s*\n)+", '\n', sentence['value'].strip())
 
     return conversations
 
 
-def _add_speaker_and_signal(header, source, get_conversation=True):
+def _add_speaker_and_signal(conversations):
     """Add speaker and start/end signal on each round."""
-    BEGIN_SIGNAL = "### "
-    END_SIGNAL = "\n"
-    conversation = header
-    for sentence in source:
+    END_SIGNAL = "\n\n"
+    for sentence in conversations:
         from_str = sentence["from"]
         if from_str.lower() == "human":
-            from_str = "USER"
+            from_str = "User"
         elif from_str.lower() == "gpt":
-            from_str = "ASSISTANT"
+            from_str = "Assistant"
         else:
-            from_str = 'unknown'
-        sentence["value"] = (BEGIN_SIGNAL + from_str + ": " +
+            raise ValueError(f"Unknown speaker: {from_str}, must be human or gpt.")
+        sentence["value"] = (from_str + ": " +
                              sentence["value"] + END_SIGNAL)
-        if get_conversation:
-            conversation += sentence["value"]
-    conversation += BEGIN_SIGNAL
-    return conversation
+    return conversations
+
+def tokenize_with_image_token(prompt, tokenizer, image_token_index=IMAGE_TOKEN_INDEX):
+    prompt_chunks = [tokenizer.encode(chunk) for chunk in prompt.split(DEFAULT_IMAGE_TOKEN)]
+
+    input_ids = []
+    for chunk in prompt_chunks:
+        input_ids.extend(chunk)
+        input_ids.append(image_token_index)
+
+    return input_ids[:-1] # remove last image token
+
+
+
+def mask_targets_from_human(targets, tokenized_lens, speakers):
+    cur_idx = 0
+    for tokenized_len, speaker in zip(tokenized_lens, speakers):
+        if speaker == "human":
+            targets[cur_idx:cur_idx + tokenized_len] = IGNORE_INDEX
+        cur_idx += tokenized_len
 
 
 def preprocess(conversations, tokenizer, has_image):
+    """
+    Given a list of sources, each is a conversation list. This transform:
+    1. Add \n\n after each round;
+    2. Concatenate conversations together;
+    3. Tokenize the concatenated conversation;
+    4. Make a deepcopy as the target. Mask human words with IGNORE_INDEX.
+    """
     # add end signal and concatenate together
-    header = f"\n\n"
-    conversation = _add_speaker_and_signal(header, conversations)
-    print(conversation)
+    conversations = _add_speaker_and_signal(conversations)
+    conversation = "".join([sentence["value"] for sentence in conversations])
+    if has_image:
+        input_ids = tokenize_with_image_token(conversation, tokenizer)
+    else:
+        input_ids = tokenizer.encode(conversation)
+    input_ids = torch.tensor(input_ids, dtype=torch.long)
+    targets = copy.deepcopy(input_ids)
+    if has_image:
+        tokenized_lens = [len(tokenize_with_image_token(s["value"], tokenizer)) for s in conversations]
+    else:
+        tokenized_lens = [len(tokenizer.encode(s["value"])) for s in conversations]
+    speakers = [sentence["from"] for sentence in conversations]
+    mask_targets_from_human(targets, tokenized_lens, speakers)
+    return dict(input_ids=input_ids, labels=targets)
 
 
 
@@ -82,20 +117,22 @@ class MyDataset(Dataset):
         if 'image' in sample:
             image_file = self.list_data_dict[idx]['image']
             image_folder = args.image_folder
-            # processor = args.image_processor
-            # image = Image.open(os.path.join(image_folder, image_file)).convert('RGB')
-            # image = processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
-            sample = process_image_tokens_in_conversations(copy.deepcopy(sample["conversations"]))
+            processor = args.image_processor
+            image = Image.open(os.path.join(image_folder, image_file)).convert('RGB')
+            image = processor.preprocess(image, return_tensors='pt')['pixel_values'][0]
+            conversations = process_image_tokens_in_conversations(copy.deepcopy(sample["conversations"]))
         else:
-            sample = copy.deepcopy(sample["conversations"])
+            conversations = copy.deepcopy(sample["conversations"])
 
         data_dict = preprocess(
-            sample,
+            conversations,
             self.tokenizer,
-            has_image=('image' in self.list_data_dict[idx]))
-
-
-        return None
+            has_image=('image' in sample))
+        
+        # image exist in the data
+        if 'image' in sample:
+            data_dict['images'] = image
+        return data_dict
     
 
 if __name__ == "__main__":
@@ -110,5 +147,9 @@ if __name__ == "__main__":
     parser.add_argument("--epoch_steps", type=int, default=100)
     parser.add_argument("--micro_bsz", type=int, default=1)
     args = parser.parse_args()
+    from rwkv.rwkv_tokenizer import TRIE_TOKENIZER
+    from transformers import CLIPImageProcessor
+    args.tokenizer = TRIE_TOKENIZER("rwkv_vocab_v20230424.txt")
+    args.image_processor = CLIPImageProcessor.from_pretrained("openai/clip-vit-base-patch32")
     dataset = MyDataset(args)
     print(dataset[0])
