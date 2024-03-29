486b48c9054a647142bf4cd361007e82c4a635b3
huohuade <houhaowen@gmail.com>
2024-03-29 22:46:40 +0800

    add exp script

--- VisualRWKV-v5/v5.0/scripts/train/rwkv7b-vitl336p14_mix665k-imglast.sh
+++ VisualRWKV-v5/v5.0/scripts/train/rwkv7b-vitl336p14_mix665k-imglast.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$(dirname "$0")")/.."
 echo "Current working directory: $(pwd)"
 
 python train.py --model_path out/rwkv7b-vitl336p14_pretrain/rwkv-17.pth \
-    --wandb "" --proj_dir out/rwkv7b-vitl336p14-577token_mix665k_zero1 \
+    --wandb "" --proj_dir out/rwkv7b-vitl336p14-577token_mix665k_imglast \
     --data_file /houhaowenT/huggingface_datasets/LLaVA-Instruct-150K/shuffled_llava_v1_5_mix665k.json \
     --data_type "json" --vocab_size 65536 \
     --ctx_len 2048 --epoch_steps 1000 --epoch_count 111 --epoch_begin 0 --epoch_save 20 \
@@ -16,5 +16,5 @@ python train.py --model_path out/rwkv7b-vitl336p14_pretrain/rwkv-17.pth \
     --accelerator gpu --devices 6 --precision bf16 --strategy deepspeed_stage_1 --grad_cp 1 \
     --image_folder /houhaowenT/huggingface_datasets/LLaVA-Instruct-150K/images/ \
     --vision_tower_name /houhaowenT/huggingface_models/openai/clip-vit-large-patch14-336 \
-    --freeze_rwkv 0 --freeze_proj 1 --detail low --grid_size -1 \
+    --freeze_rwkv 0 --freeze_proj 1 --detail low --grid_size -1 --image_position last \
     --enable_progress_bar False
