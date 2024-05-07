4e5bafa6e0c77c5c1a8fa14843eaa3dd9b5e583a
huohuade <houhaowen@gmail.com>
2024-05-07 22:56:42 +0800

    Update model.py

--- VisualRWKV-v6/v6.0/src/model.py
+++ VisualRWKV-v6/v6.0/src/model.py
@@ -115,6 +115,8 @@ class RWKV_Tmix_x060(MyModule):
             self.time_maa_g = nn.Parameter(1.0 - torch.pow(ddd, 0.5 * ratio_1_to_almost0))
 
             D_MIX_LORA = 32 # generate TIME_MIX for w,k,v,r,g
+            if args.n_embd >= 4096:
+                D_MIX_LORA = 64
             self.time_maa_w1 = nn.Parameter(torch.zeros(args.n_embd, D_MIX_LORA*5))
             self.time_maa_w2 = nn.Parameter(torch.zeros(5, D_MIX_LORA, args.n_embd).uniform_(-0.01, 0.01))
 
@@ -125,6 +127,8 @@ class RWKV_Tmix_x060(MyModule):
             self.time_decay = nn.Parameter(decay_speed.reshape(1,1,args.dim_att))
 
             D_DECAY_LORA = 64
+            if args.n_embd >= 4096:
+                D_DECAY_LORA = 128
             self.time_decay_w1 = nn.Parameter(torch.zeros(args.n_embd, D_DECAY_LORA))
             self.time_decay_w2 = nn.Parameter(torch.zeros(D_DECAY_LORA, args.dim_att).uniform_(-0.01, 0.01))
 
