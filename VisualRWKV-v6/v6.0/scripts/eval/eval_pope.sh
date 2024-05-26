28ac8263020c511840c8a789266b54bc0bca4d4d
huohuade <houhaowen@gmail.com>
2024-05-26 16:29:12 +0800

    x

--- VisualRWKV-v6/v6.0/scripts/eval/eval_pope.sh
+++ VisualRWKV-v6/v6.0/scripts/eval/eval_pope.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+# 
+gpu_list="${CUDA_VISIBLE_DEVICES:-0}"
+IFS=',' read -ra GPULIST <<< "$gpu_list"
+
 model_path=$1
 ctx_len=$2
 grid_size=$3
@@ -26,16 +31,32 @@ echo "ctx_len: $ctx_len, grid_size: $grid_size, n_embd: $n_embd, n_layer: $n_lay
 echo "eval dir: $eval_dir"
 echo "vision tower path: $vision_tower_path", "image_position: $image_position"
 
-python evaluate.py \
-    --ctx_len $ctx_len --grid_size $grid_size --n_embd $n_embd --n_layer $n_layer \
-    --vision_tower_name $vision_tower_path \
-    --model_path $model_path \
-    --image_folder $eval_dir/eval/pope/val2014 \
-    --question_file $eval_dir/eval/pope/llava_pope_test.jsonl \
-    --output_file $eval_dir/eval/pope/answers/$exp_name.jsonl \
-    --image_position $image_position
+mkdir -p $eval_dir/eval/pope/answers/$exp_name
+CHUNKS=${#GPULIST[@]}
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]}
+    python evaluate.py \
+        --ctx_len $ctx_len --grid_size $grid_size --n_embd $n_embd --n_layer $n_layer \
+        --vision_tower_name $vision_tower_path \
+        --model_path $model_path \
+        --image_folder $eval_dir/eval/pope/val2014 \
+        --question_file $eval_dir/eval/pope/llava_pope_test.jsonl \
+        --output_file $eval_dir/eval/pope/answers/$exp_name/${CHUNKS}_${IDX}.jsonl \
+        --image_position $image_position \ 
+        --num_chunks $CHUNKS \
+        --chunk_idx $IDX &
+    echo "Started chunk $IDX"
+done
+wait
+
+output_file=$eval_dir/eval/pope/answers/${exp_name}/merge.jsonl
+> "$output_file"
+
+for IDX in $(seq 0 $((CHUNKS-1))); do
+    cat $eval_dir/eval/pope/answers/${exp_name}/${CHUNKS}_${IDX}.jsonl >> "$output_file"
+done
 
 python eval/eval_pope.py \
     --annotation-dir $eval_dir/eval/pope/coco \
     --question-file $eval_dir/eval/pope/llava_pope_test.jsonl \
-    --result-file $eval_dir/eval/pope/answers/$exp_name.jsonl
+    --result-file $eval_dir/eval/pope/answers/$exp_name/merge.jsonl
