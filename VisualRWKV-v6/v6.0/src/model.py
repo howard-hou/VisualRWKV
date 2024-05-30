37c9841c649aa2a72535c6449149f37d003578b8
huohuade <houhaowen@gmail.com>
2024-05-30 14:12:11 +0800

    Update model.py

--- VisualRWKV-v6/v6.0/src/model.py
+++ VisualRWKV-v6/v6.0/src/model.py
@@ -379,11 +379,19 @@ class VisualRWKV(pl.LightningModule):
         self.proj.requires_grad_(False)
 
     def configure_optimizers(self):
-        trainable_params = [p for p in self.parameters() if p.requires_grad]
+        zero_weight_decay_group = [p for p in self.parameters() if not isinstance(p, nn.Linear)]
+        # add weight decay to nn.Linear
+        weight_decay_group = [p for p in self.parameters() if isinstance(p, nn.Linear)] 
+
         name_of_trainable_params = [n for n, p in self.named_parameters() if p.requires_grad]
         rank_zero_info(f"Name of trainable parameters in optimizers: {name_of_trainable_params}")
-        rank_zero_info(f"Number of trainable parameters in optimizers: {len(trainable_params)}")
-        optim_groups = [{"params": trainable_params, "weight_decay": self.args.weight_decay}]
+        rank_zero_info(f"Number of trainable parameters in optimizers: {len(name_of_trainable_params)}")
+        optim_groups = [{"params": zero_weight_decay_group, "weight_decay": 0.0}]
+        if self.args.weight_decay > 0:
+            optim_groups += [{"params": weight_decay_group, "weight_decay": self.args.weight_decay}]
+            rank_zero_info(f"Number of parameters with weight decay: {len(weight_decay_group)}, with value: {self.args.weight_decay}")
+        else:
+            optim_groups += [{"params": weight_decay_group, "weight_decay": 0.0}] 
         if self.deepspeed_offload:
             return DeepSpeedCPUAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, adamw_mode=True, amsgrad=False)
         return FusedAdam(optim_groups, lr=self.args.lr_init, betas=self.args.betas, eps=self.args.adam_eps, bias_correction=True, adam_w_mode=True, amsgrad=False)
