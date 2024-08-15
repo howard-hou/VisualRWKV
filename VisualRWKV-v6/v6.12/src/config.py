a1f785364c9e7981798a4a708be93688335858d7
huohuade <houhaowen@gmail.com>
2024-08-15 18:56:38 +0800

    Update config.py

--- VisualRWKV-v6/v6.12/src/config.py
+++ VisualRWKV-v6/v6.12/src/config.py
@@ -4,5 +4,5 @@ VISION_TOWER_CHECKPOINT_NAMES = {
     "dino": "timm/vit_large_patch14_reg4_dinov2.lvd142m/pytorch_model.bin",
     "siglip": "timm/ViT-SO400M-14-SigLIP-384/open_clip_pytorch_model.bin",
     "siglip2": "timm/ViT-B-16-SigLIP-384/open_clip_model.safetensors",
-    "sam": "facebook/sam_vit_b_01ec64.pth"
+    "sam": "facebook/sam-vit-base/sam_vit_b_01ec64.pth"
 }
