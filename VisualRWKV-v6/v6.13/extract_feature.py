fb65ebf7bc2f75f7b5607b4b6f86c8cd4add1223
Haowen Hou <houhaowen@gmail.com>
2024-09-01 20:50:42 +0800

    Update extract_feature.py

--- VisualRWKV-v6/v6.13/extract_feature.py
+++ VisualRWKV-v6/v6.13/extract_feature.py
@@ -32,6 +32,8 @@ if __name__ == "__main__":
     # must set shuffle=False, persistent_workers=False (because worker is in another thread)
     data_loader = DataLoader(train_data, shuffle=False, pin_memory=True, batch_size=args.micro_bsz, num_workers=1, 
                              persistent_workers=False, drop_last=False)
+    image_feature_folder = Path(args.image_feature_folder)
+    image_feature_folder.mkdir(exist_ok=True, parents=True)
     # extract feature, save to disk
     for batch in tqdm(data_loader, desc="Extracting feature"):
         model.predict_step(batch)
