a077b5a66cf1c7fc0e2bcf22614c39894178ffd2
huohuade <houhaowen@gmail.com>
2024-09-03 11:47:22 +0800

    Update visualrwkv_eval.py

--- VisualRWKV-v6/v6.20/eval/visualrwkv_eval.py
+++ VisualRWKV-v6/v6.20/eval/visualrwkv_eval.py
@@ -1,11 +1,14 @@
 import argparse
 import json
+import random
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('annotation_file', type=str)
     parser.add_argument('result_file', type=str)
+    # print badcase
+    parser.add_argument('--print_badcase', action='store_true')
     return parser.parse_args()
 
 
@@ -17,18 +20,27 @@ def eval_single(annotation_file, result_file):
     pred_list = []
     for result in results:
         annotation = annotations[result['question_id']]
-        answers = [ans.strip().lower() for ans in annotation['answers']]
+        answers = [ans.strip().lower().strip('.') for ans in annotation['answers']]
         pred_list.append({
-            "pred_answer": result['text'].strip().lower(),
+            "pred_answer": result['text'].strip().lower().strip('.'),
             "gt_answers": answers,
         })
 
     correct = 0
+    badcase_list = []
     for pred in pred_list:
-        if pred['pred_answer'] in pred['gt_answers']:
+        pred_answer = pred['pred_answer']
+        if pred_answer in pred['gt_answers']:
             correct += 1
+        else:
+            badcase_list.append(dict(pred_answer=pred_answer, gt_answers=pred['gt_answers']))
     acc = correct / len(pred_list)
     print("result_file: {}, acc: {:.2f}%".format(result_file, acc * 100))
+    if args.print_badcase:
+        # sample 10 from badcase_list
+        badcase_sampled = random.sample(badcase_list, min(10, len(badcase_list)))
+        for i, badcase in enumerate(badcase_sampled):
+            print("badcase {}: pred: {}, gt: {}".format(i, badcase['pred_answer'], badcase['gt_answers']))
 
 
 if __name__ == "__main__":
