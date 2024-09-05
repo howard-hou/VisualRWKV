7559c61db2cb011acc6d0d5bb45e90f0d919df14
Haowen Hou <houhaowen@gmail.com>
2024-09-06 00:47:06 +0800

    Update download_huggingface.py

--- download_huggingface.py
+++ download_huggingface.py
@@ -2,7 +2,7 @@ from huggingface_hub import snapshot_download
 
 snapshot_download(repo_id="lmms-lab/M4-Instruct-Data", 
                   repo_type="dataset",
-                  local_dir="D:\huggingface_datasets",
+                  local_dir="D:\huggingface_datasets\lmms-lab/M4-Instruct-Data",
                   allow_patterns=["*.zip", "*.json", "*.z01"],
                   ignore_patterns=["*.h5", "*.ot", "*.msgpack"],
                   resume_download=True,
