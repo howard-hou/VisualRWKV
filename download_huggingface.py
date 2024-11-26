b9285b4b8463f19c3ddb6f929a7e3784a5c6f89d
Haowen Hou <houhaowen@gmail.com>
2024-11-26 21:56:48 +0800

    x

--- download_huggingface.py
+++ download_huggingface.py
@@ -1,8 +1,9 @@
-from huggingface_hub import snapshot_download
+from huggingface_hub import snapshot_download, hf_hub_download
 
-snapshot_download(repo_id="ShareGPTVideo/train_video_and_instruction", 
+snapshot_download(repo_id="lmms-lab/LLaVA-Video-178K", 
                   repo_type="dataset",
-                  local_dir="D:\huggingface_datasets\ShareGPTVideo",
-                  allow_patterns=["*.zip", "*.json", "*.z01", "*.tar.gz"],
+                  local_dir="D:\huggingface_datasets\LLaVA-Video-178K",
+                  allow_patterns=["30_60_s_youtube_v0_1/*"],
+                  #allow_patterns=["*.zip", "*.json", "*.z01", "*.tar.gz"],
                   ignore_patterns=["*.h5", "*.ot", "*.msgpack"],
                   local_dir_use_symlinks=False)
