1b450dc540d1fd5f73582f9b82acdf8c2eba6d66
Haowen Hou <houhaowen@gmail.com>
2025-01-04 23:30:23 +0800

    Update rwkv0b1_mix665k.sh

--- VisualRWKV-v7/v7.10/scripts/train/rwkv0b1_mix665k.sh
+++ VisualRWKV-v7/v7.10/scripts/train/rwkv0b1_mix665k.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$(dirname "$0")")/.."
 echo "Current working directory: $(pwd)"
 
 
-python train.py --model_path out/rwkv0b1-v0700_pretrain/rwkv-1.pth \
+python train.py --model_path out/rwkv0b1-v0710_pretrain/rwkv-1.pth \
     --wandb "rwkv0b1-v0710_mix665k" --proj_dir out/rwkv0b1-v0710_mix665k \
     --data_file /home/rwkvos/howardhwhou/LLaVA-Instruct-150K/shuffled_llava_v1_5_mix665k_5rounds.json \
     --data_type "json" --vocab_size 65536 \
@@ -17,5 +17,4 @@ python train.py --model_path out/rwkv0b1-v0700_pretrain/rwkv-1.pth \
     --lr_init 6e-5 --lr_final 1.5e-5 --warmup_steps 0 --beta1 0.9 --beta2 0.99 --adam_eps 1e-8 \
     --accelerator gpu --devices 8 --precision bf16 --strategy deepspeed_stage_1 --grad_cp 1 \
     --image_folder /home/rwkvos/howardhwhou/LLaVA-Instruct-150K/images \
-    --vision_tower_dir /home/rwkvos/howardhwhou/huggingface_models/ \
-    --freeze_rwkv 0 --freeze_proj 0  --num_token_per_image 1024 --proj_type mlp
+    --image_size 256 --patch_size 16
